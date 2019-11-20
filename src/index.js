6896a518a883cf7ac25bc4a26fe7112cfa270ce3
Nehal Udyavar <nehaludyavar@gmail.com>
2019-11-20 02:20:32 -0800

    Create Track model and GET and POST routes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ const bodyParser = require("body-parser");
 const keys = require("./config/dev.js");
 
 require("./models/User");
+require("./models/Track");
 
 const requireAuth = require("./middleware/requireAuth");
 const authRoutes = require("./routes/authRoutes");
+const trackRoutes = require("./routes/trackRoutes");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 mongoose.connect(keys.mongoUri, {
   useNewUrlParser: true,
